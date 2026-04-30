51fa75bd08a67305ce7d09c570f5ac39035b5a27
fangyung0323 <fangyung0323@gmail.com>
2026-04-30 14:48:18 +0800

    Update main.py

--- scripts/main.py
+++ scripts/main.py
@@ -1115,10 +1115,11 @@ def generate_daily_post_index(daily_post_dir):
                     </div>"""
     
     # 過往文章列表
-    past_list_html = ""
-    for article in past_articles[:10]:
-        past_list_html += f"""
+   past_list_html = ""
+for article in past_articles[:10]:
+    past_list_html += f"""
                         <li class="past-item">
+                            <span class="past-badge" style="display:none;">{article['category']}</span>
                             <a class="past-link" href="{article['filename']}">{article['title']}</a>
                             <div class="past-meta">📅 {article['date']} · {article['category']}</div>
                         </li>"""
